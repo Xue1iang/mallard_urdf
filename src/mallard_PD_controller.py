1b820ba04e94b3c4ca23bedbcba95c559fdbc14d
konrad <konrad.gornicki@yahoo.com>
2020-06-01 15:30:24 +0100

    tested by rostopic hz control_callback. The time error is about 100 times less than period of 0.1 s.

--- src/mallard_PD_controller.py
+++ src/mallard_PD_controller.py
@@ -17,11 +17,18 @@ from statistics import mean,stdev
 
 
 
-# variables for velocity calculations:
+# slam position and velocity variables:
+x = 0
+y = 0
+psi = 0
+x_vel = 0
+y_vel = 0
+psi_vel = 0
 time_prev = 0
 x_prev = 0
 y_prev = 0
 psi_prev = 0
+
 # varaibles to store goals:
 goals_received = False
 x_goal       = 0
@@ -30,6 +37,9 @@ psi_goal     = 0
 x_vel_goal   = 0
 y_vel_goal   = 0
 psi_vel_goal = 0
+# timer callback test variables:
+time_p = 0
+period_list = []
 
 # simulation variables:
 a_sim=1.0556
@@ -60,8 +70,8 @@ def thruster_ctrl_msg():
 
 # ------ Callbacks -----
 
-# Publishing node: mallard_goal_selector.py, topic: /mallard/goals
 def goal_callback(array):
+    # Publishing node: mallard_goal_selector.py, topic: /mallard/goals
     global goals_received, x_goal, y_goal,psi_goal
     global x_vel_goal,y_vel_goal,psi_vel_goal
     
@@ -77,21 +87,11 @@ def goal_callback(array):
         y_vel_goal   = array.data[5]
         psi_vel_goal = array.data[6]
 
-# Publishing node: hector_slam, topic: /slam_out_pose
+
 def slam_callback(msg):
-    global time_prev,x_prev,y_prev,psi_prev
-    global thruster_1, thruster_2, thruster_3, thruster_4
-    
-    # ----- Goals recived? -----
-    if(goals_received == False):
-        thruster_1 = 0
-        thruster_2 = 0
-        thruster_3 = 0
-        thruster_4 = 0
-        pub_velocity.publish(thruster_ctrl_msg())
-        # print("No goals received, idle. ")
-        # Turn off thrustrs and exit callback
-        return
+    # Publishing node: hector_slam, topic: /slam_out_pose
+    global x,y,psi,x_vel,y_vel,psi_vel # slam position and derived velocity
+    global time_prev,x_prev,y_prev,psi_prev # variables for next iteration
 
     # ----- Position and Velocity -----
     # Get current position, orientation and time:
@@ -100,63 +100,59 @@ def slam_callback(msg):
     psi = tft.euler_from_quaternion([msg.pose.orientation.x,msg.pose.orientation.y,\
                                      msg.pose.orientation.z,msg.pose.orientation.w])[2]
     time = (msg.header.stamp.secs + msg.header.stamp.nsecs * 0.000000001)
+
     # Derive velocities from slam data:
     time_diff = time - time_prev
     x_vel   = control.get_velocity(x,x_prev,time_diff)
     y_vel   = control.get_velocity(y,y_prev,time_diff)
     psi_vel = control.get_velocity(psi,psi_prev,time_diff)
 
-    # ----- Control -----
-    #  Get forces in global frame using PD controller
-    x_global_ctrl   = control.proportional(x, x_goal, x_vel, x_vel_goal, param['kp'], param['kd'], param['lim'])
-    y_global_ctrl   = control.proportional(y, y_goal, y_vel, y_vel_goal, param['kp'], param['kd'], param['lim'])
-    psi_global_ctrl = control.proportional_angle(psi, psi_goal,psi_vel,psi_vel_goal, param['kp_psi'], param['kd_psi'], param['lim_psi'])
-    # convert into body frame:
-    x_body_ctrl =  math.cos(psi)*x_global_ctrl + math.sin(psi)*y_global_ctrl
-    y_body_ctrl = -math.sin(psi)*x_global_ctrl + math.cos(psi)*y_global_ctrl
+    # control place holder if timer_callback inactive
     
-    # ----- Simulation -----
-    # vector forces scaled in body frame
-    x_sim   = (x_body_ctrl)*linear_scale
-    y_sim   = (y_body_ctrl)*linear_scale
-    psi_sim = (-psi_global_ctrl)*angular_scale
-
-    # thrust allocation
-    thruster_1 = 0 + 0.5*x_sim + a_sim*psi_sim
-    thruster_2 = 0 + 0.5*x_sim - a_sim*psi_sim
-    thruster_3 = 0 - 0.5*y_sim + b_sim*psi_sim
-    thruster_4 = 0 - 0.5*y_sim - b_sim*psi_sim
-    # Publish forces to simulation (joint_state_publisher message)
-    pub_velocity.publish(thruster_ctrl_msg())
-
-    # ----- Next iteratioon -----
-    # Before finishing, assign current values to previous ones
+    # ----- for next iteratioon -----
     time_prev = time
     x_prev    = x
     y_prev    = y
     psi_prev  = psi
 
-time_p = 0
-period_list = []
-def timer_callback(event):
-    global time_p,period_list
+def control_callback(event):
+    # rospy.Timer() callback trigered by Duration(event).
+    # Actual control is done here. The 'event' is the rospy.Timer() duration period, can be used 
+    # for trubleshooting. To test how often is executed use: $ rostopic hz /mallard/thruster_commands.
 
-    # ----- Loop timing test, measures how precise it is -----
-    t = rospy.Time.from_sec(time.time())
-    time_now = t.to_sec() + t.to_nsec()
+    global thruster_1, thruster_2, thruster_3, thruster_4 # control forces
 
-    period = (time_now - time_p)*10**-9 # convert to seconds 
+    # ----- Control -----
+    #  Get forces in global frame using PD controller
+    if(goals_received == True):
+        
+        x_global_ctrl   = control.proportional(x, x_goal, x_vel, x_vel_goal, param['kp'], param['kd'], param['lim'])
+        y_global_ctrl   = control.proportional(y, y_goal, y_vel, y_vel_goal, param['kp'], param['kd'], param['lim'])
+        psi_global_ctrl = control.proportional_angle(psi, psi_goal,psi_vel,psi_vel_goal, param['kp_psi'], param['kd_psi'], param['lim_psi'])
+        # convert into body frame:
+        x_body_ctrl =  math.cos(psi)*x_global_ctrl + math.sin(psi)*y_global_ctrl
+        y_body_ctrl = -math.sin(psi)*x_global_ctrl + math.cos(psi)*y_global_ctrl
+        
+        # ----- Simulation -----
+        # vector forces scaled in body frame
+        x_sim   = (x_body_ctrl)*linear_scale
+        y_sim   = (y_body_ctrl)*linear_scale
+        psi_sim = (-psi_global_ctrl)*angular_scale
 
-    period_list.append(period)
-    if(len(period_list)>2):
-        # first element in time stamp; start with second
-        period_mean = mean(period_list[1:])
-        print("mean period: ","{:.5f}".format(period_mean))
-        period_stdev = stdev(period_list[1:])
-        print("std dev: ","{:.6f}".format(period_stdev))
-        # print("std devation :" + f"{period_stdev.9f}")
-    time_p = time_now
-    # ----- end of test -----
+        # thrust allocation
+        thruster_1 = 0 + 0.5*x_sim + a_sim*psi_sim
+        thruster_2 = 0 + 0.5*x_sim - a_sim*psi_sim
+        thruster_3 = 0 - 0.5*y_sim + b_sim*psi_sim
+        thruster_4 = 0 - 0.5*y_sim - b_sim*psi_sim
+
+        # Publish forces to simulation (joint_state_publisher message)
+        pub_velocity.publish(thruster_ctrl_msg())
+    else:
+        thruster_1 = 0
+        thruster_2 = 0
+        thruster_3 = 0
+        thruster_4 = 0
+        pub_velocity.publish(thruster_ctrl_msg())
 
 
 if __name__ == '__main__':
@@ -165,12 +161,8 @@ if __name__ == '__main__':
     pub_velocity = rospy.Publisher('/mallard/thruster_command',JointState,queue_size=10)
 
     # SUBSCRIBER
-    # rospy.Subscriber("/mallard/goals", Float64MultiArray, goal_callback)  # subscribes to topic "/slam_out_pose"
     rospy.Subscriber("/slam_out_pose",PoseStamped,slam_callback)
     rospy.Subscriber("/mallard/goals",Float64MultiArray,goal_callback)
-    # Subscribe to array of goal poses from RVIZ interactive coverage selector
-    # dynrecon = Server(MtwoParamConfig)
-
-    rospy.Timer(rospy.Duration(0.01), timer_callback,oneshot=False)
+    rospy.Timer(rospy.Duration(0.1), control_callback,oneshot=False)
 
     rospy.spin()
