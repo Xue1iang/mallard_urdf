5a5b531c0cfe3c99ae409151b64cf4d6c999e593
Xue1iang <77399327+Xue1iang@users.noreply.github.com>
2021-06-28 10:08:56 +0100

    Using joystick to control the MALLARD in gazebo
    Using joystick to control the MALLARD in gazebo

--- src/joy2mallard.py
+++ src/joy2mallard.py
@@ -5,7 +5,6 @@ from sensor_msgs.msg import JointState
 
 
 # Receives joystick messages (subscribed to Joy topic)
-# then converts the joysick inputs into Twist commands
 # axis 1 aka left stick vertical
 # axis 0 aka left stick horizonal
 
